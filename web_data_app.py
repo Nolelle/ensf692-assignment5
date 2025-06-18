bc8c87f8098299ac3d9e2a584b90f3e47fb26f5d
Edmund <eyu1@ualberta.ca>
2025-06-18 00:34:43 -0600

    finish work

--- web_data_app.py
+++ web_data_app.py
@@ -1,32 +1,30 @@
 # web_data_app.py
 # June 2025
-# Modified by: STUDENT NAME
+# Modified by: Edmund Yu
 #
 # An simple program for demonstrating web applications using Flask and web scraping of data using Beautiful Soup.
 # Detailed specifications are provided via the Assignment 5 README file.
 
 
-import pandas as pd     # needed for data manipulation
-
-from flask import Flask, render_template    # needed for web app
-
-from bs4 import BeautifulSoup               # needed for web scraping
-import requests
-
-from datetime import datetime               # needed for time/regular expressions
 import re
+from datetime import datetime  # needed for time/regular expressions
 
+import pandas as pd  # needed for data manipulation
+import requests
+from bs4 import BeautifulSoup  # needed for web scraping
+from flask import Flask, render_template  # needed for web app
 
 ###
 # Initialize our FLASK application object from the Flask class like so:
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     """
-    Our first (topmost) route (aka 'view').  Routes are like URLs, 
-    which we can access from a browser. The line immediately below is known as a 
-    'decorator' and implements some boilerplate code for us without much work on 
+    Our first (topmost) route (aka 'view').  Routes are like URLs,
+    which we can access from a browser. The line immediately below is known as a
+    'decorator' and implements some boilerplate code for us without much work on
     our end.  Basically it creates a 'view' to our app and data to show the user.
     This one merely shows a static message.  Easy-peasy.
     """
@@ -36,9 +34,9 @@ def index():
 @app.route("/hello/<name>")
 def hello_there(name):
     """
-    This route shows a bit more of what we can do, including some 'REST'full user 
+    This route shows a bit more of what we can do, including some 'REST'full user
     interaction.  If a user specifies a name in the url like so: http://localhost:5000/hello/supercoder
-    the response will include the value 'supercoder' in the `name` variable, and 
+    the response will include the value 'supercoder' in the `name` variable, and
     can then be validated with the RE `match_object` output.  If an invalid value is specified
     for `name` the validator code will simply assign 'friend' value to the `clean_name`
     instead.  If no value is specified, it will error out and present a 404 error to the browser.
@@ -55,7 +53,12 @@ def hello_there(name):
         clean_name = "Friend"
 
     # Output the semi-static text with the time and supplied name (if any)
-    content = "Hello there, " + clean_name + "! Welcome to Assignment 5. It's " + formatted_now
+    content = (
+        "Hello there, "
+        + clean_name
+        + "! Welcome to Assignment 5. It's "
+        + formatted_now
+    )
     return content
 
 
@@ -65,30 +68,43 @@ def book_data():
     Now let's take what we learned about Pandas and scrap some data from the internet!
     """
 
-    # Web scraping can be against the Terms of Use. 
+    # Web scraping can be against the Terms of Use.
     # Always check to make sure that you are web scraping legally and ethically.
     # The following site was specifically created to practice web scraping.
     source = requests.get("http://books.toscrape.com/")
-    soup = BeautifulSoup(source.content, 'html.parser')     # Use BeautifulSoup to parse the website html code
-    book_results = soup.find_all(attrs={'class':'product_pod'})  # By inspecting the site, we know books = product_pod class
+    soup = BeautifulSoup(
+        source.content, "html.parser"
+    )  # Use BeautifulSoup to parse the website html code
+    book_results = soup.find_all(
+        attrs={"class": "product_pod"}
+    )  # By inspecting the site, we know books = product_pod class
 
     titles = []
     prices = []
 
     # For each book listed on the page, get the title and the price from inside in the html data
     for book in book_results:
-        titles.append(book.h3.a.get('title'))
-        prices.append(float(book.find('p', class_="price_color").text[1:]))
+        titles.append(book.h3.a.get("title"))
+        prices.append(float(book.find("p", class_="price_color").text[1:]))
+
+    sale_prices = [price * 0.75 for price in prices]
 
     # Create a DataFrame using the two lists
-    book_data = pd.DataFrame(list(zip(titles, prices)), columns=['Titles','Prices'])    
-    print(book_data)        # Print to the terminal as confirmation - only we can see this
+    book_data = pd.DataFrame(
+        list(zip(titles, prices, sale_prices)),
+        columns=["Titles", "Prices", "Sale_Prices"],
+    )
+    print(book_data)  # Print to the terminal as confirmation - only we can see this
 
     # Format and print the DataFrame using the html template provided in the templates subdirectory
-    return render_template('template.html',  tables=[book_data.to_html(classes='data')], titles=book_data.columns.values)
+    return render_template(
+        "template.html",
+        tables=[book_data.to_html(classes="data")],
+        titles=book_data.columns.values,
+    )
+
 
 @app.route("/learn")
 def learn():
     # Return a string the describes one thing you learned in ENSF 692.
-    pass
-
+    return "I learned how to use Dataframes, Series, and Index objects in Pandas."
